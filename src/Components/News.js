1f251952ae7f5602d2d4228a6c4f909b4bd38cf7
Mihir Waykole <mihirwaykole1703@yahoo.com>
2021-09-30 10:04:15 +0530

    last commit and deployed

--- src/Components/News.js
+++ src/Components/News.js
@@ -2,39 +2,38 @@ import React, { Component } from 'react'
 import NewsComponent from './NewsComponent'
 
 export class News extends Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
-            articles : [],
-            loading : false,
-            page:1
+            articles: [],
+            loading: false
         }
     }
-    async componentDidMount(){
-        let url = `https://node.mihirwaykole.repl.co/${this.state.page}`
+    async componentDidMount() {
+        let url = `https://node.mihirwaykole.repl.co/`
         let data = await fetch(url)
         let parsedData = await data.json()
         console.log(parsedData)
-        this.setState({articles : parsedData.articles})
+        this.setState({ articles: parsedData.articles })
     }
-    HandlePrevClick = async() => {
-        let url = `https://node.mihirwaykole.repl.co/${this.state.page}`
+    HandlePrevClick = async () => {
+        let url = `https://node.mihirwaykole.repl.co/`
         let data = await fetch(url)
         let parsedData = await data.json()
         console.log(parsedData)
         this.setState({
-            page:this.state.page + 1,
-            articles : parsedData.articles
+            page: this.state.page + 1,
+            articles: parsedData.articles
         })
     }
-    HandleNextClick = async() => {
+    HandleNextClick = async () => {
         console.log("clickd")
-        let url = `https://node.mihirwaykole.repl.co/${this.state.page+1}`
+        let url = `https://node.mihirwaykole.repl.co/`
         let data = await fetch(url)
         let parsedData = await data.json()
         this.setState({
-            page:this.state.page - 1,
-            articles : parsedData.articles
+            page: this.state.page - 1,
+            articles: parsedData.articles
         })
     }
     render() {
@@ -42,21 +41,18 @@ export class News extends Component {
             <div className="container my-3 ">
                 <h2>News-Today - Top Headlines only for You!</h2>
                 <div className="row d-flex justify-content-center">
-                    {this.state.articles.map((element)=>{
+                    {this.state.articles.map((element) => {
                         if (element.urlToImage) {
                             return <div className="col-md-3" key={element.url}>
-                        <NewsComponent title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage?element.urlToImage:"https://img.icons8.com/nolan/64/no-image.png"} newsUrl={element.url}></NewsComponent>
-                        </div>
+                                <NewsComponent title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage ? element.urlToImage : "https://img.icons8.com/nolan/64/no-image.png"} newsUrl={element.url}></NewsComponent>
+                            </div>
                         }
                     })}
-                    
+
                 </div>
                 <div className="container">
-                <div class="d-flex justify-content-around">
-
-                <button type="button" disabled={this.state.page<=1} class="btn btn-dark" onClick={this.HandlePrevClick}>&larr; Previous</button>
-                <button type="button"  class="btn btn-dark" onClick={this.HandleNextClick}>Next &rarr;</button>
-                </div>
+                    <div class="d-flex justify-content-around">
+                    </div>
                 </div>
             </div>
         )
